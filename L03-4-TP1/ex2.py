6505d8f2b797cdecc76f9a9318834540d4956bcc
Reginald Rancy <reggiehr_30@hotmail.com>
2024-09-10 11:04:58 -0400

    Exercice 2 terminé

--- L03-4-TP1/ex2.py
+++ L03-4-TP1/ex2.py
@@ -1,4 +1,9 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+water_quantity = input("Quelle quantité d'eau faut-il assainir? ")
+ratio = int(water_quantity)/5
+print(f"Voici les éléments requis pour assainir {water_quantity}L d'eau:")
+print(f"\t- Filtres: {ratio}")
+print(f"\t- Lampes UV: {ratio *3}")
+print(f"\t- Chlore: {ratio * 0.5}kg")
