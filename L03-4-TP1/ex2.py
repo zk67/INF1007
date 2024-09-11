3cf5ee5a6b6df8243ddb3b2d13b0297d95671efa
Reginald Rancy <reggiehr_30@hotmail.com>
2024-09-11 17:03:07 -0400

    Correction format des questions ex2

--- L03-4-TP1/ex2.py
+++ L03-4-TP1/ex2.py
@@ -1,10 +1,19 @@
+import math
+
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = input("Quelle quantité d'eau faut-il assainir? ")
-ratio = int(water_quantity)/5
-print(f"Voici les éléments requis pour assainir {water_quantity}L d'eau:")
-print(f"\t- Filtres: {ratio}")
-print(f"\t- Lampes UV: {ratio *3}")
-print(f"\t- Chlore: {ratio * 0.5}kg")
+water_quantity = input("Quelle quantité d'eau faut-il assainir ? ")
+ratio = float(water_quantity)/5
+nbfiltres = ratio
+nblampes = 3*ratio
+if nbfiltres-int(nbfiltres) > 0:nbfiltres +=1
+if nblampes-int(nblampes) > 0:nblampes += 1
 
+print(f"""Voici les éléments requis pour assainir {water_quantity}L d'eau:\n
+        \t- Filtre(s) : {int(nbfiltres)}\n
+        \t- Lampe(s) UV : {int(nblampes)}\n
+        \t- Chlore : {ratio * 0.5}kg""")
+
+
+    
