75ff52aff57dfeaa540d9a3e03aad2b16cbc8138
Reginald Rancy <reggiehr_30@hotmail.com>
2024-09-12 16:17:04 -0400

    Ex4 Complété mais à réviser

--- L03-4-TP1/ex4.py
+++ L03-4-TP1/ex4.py
@@ -4,25 +4,34 @@ import math
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = float(input("Quel est le pourcentage de la batterie ? "))
+battery_level = float(input("Pourcentage de batterie ? "))
 
 distance = 0
-if(battery_level == 0): print("La batterie est vide\n")
+printDistance = True
+
+if(battery_level == 0): 
+    print("La batterie est vide")
+    printDistance = False
 while battery_level > 50:
-    distance += 2
-    battery_level -= 1
-while battery_level > 25:
-    distance += 0.5
-    battery_level -= 1
-while battery_level > 10:
     distance += 1
-    battery_level -= 1
+    battery_level -= .5
+    #print(distance, battery_level)
+while battery_level > 25:
+    distance += 0.25
+    battery_level -= 0.5
+   # print(distance, battery_level)
+while battery_level > 10:
+    distance += .5
+    battery_level -= .5
+    #print(distance, battery_level)
 while battery_level > 5:
-    distance += 2.5
-    battery_level -= 1
+    distance += 1.25
+    battery_level -= 0.5
+    #print(distance, battery_level)
 while battery_level > 0:
-    distance += 6
-    battery_level -= 1
+    distance += 3
+    battery_level -= 0.5
+    #print(distance, battery_level)
 
-
-print(f"Distance pouvant être parcourue: {math.trunc(distance)} km")
+# On affiche la distance seulement si la batterie est non vide
+if(printDistance == True):print(f"{round(distance,1)} km")
